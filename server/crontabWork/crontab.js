18f4613092885d69b21576bbd1d7a5f67aed965e
NeoNeoHo <b95042@gmail.com>
2015-12-30 15:55:55 +0800

    add schedule

--- server/crontabWork/crontab.js
+++ server/crontabWork/crontab.js
@@ -7,8 +7,11 @@ var db_config = require('../config/db_config.js');
 var mysql_connection = db_config.mysql_connection;
 var mysql_pool = db_config.mysql_pool;
 var invoice = require('../api/invoice/invoice.controller.js');
+var schedule = require('node-schedule');
 
-var date = new Date();
-var yesterday = date.setDate(date.getDate() - 1);
-
-var result = setInterval(function(){invoice.AutoCreateInvoiceNo(yesterday)}, 3000);
+var j = schedule.scheduleJob({hour: 16, minute: 0}, function(){
+	var date = new Date();
+	var yesterday = date.setDate(date.getDate() - 1);
+	invoice.AutoCreateInvoiceNo(yesterday)
+  	console.log('Update Invoice!  ' + date);
+});
